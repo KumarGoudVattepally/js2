cc9e0aa00d6b7261b3c2d09a3280390a602fd59e
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-10-21 19:30:15 +0530

    addd

---
+++ index.js
@@ -0,0 +1,26 @@
+let x=5;
+let y=x;
+x=10;
+console.log(x);//10
+console.log(y);//5
+let obj1={name:"kumar"}
+let obj2=obj1;
+obj1.name="bob"
+console.log(obj1.name);//bob
+console.log(obj2.name);//bob
+let a="hii";
+let b=42;
+let c=true;
+let d={key:"value"};
+let e=null;
+let f=undefined;
+console.log(typeof a);//string
+console.log(typeof b);//number
+console.log(typeof c);//boolean
+console.log(typeof d);//object
+console.log(typeof e);//object
+console.log(typeof f);//undefined
+
+
+
+
